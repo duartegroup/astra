13aed8ff3653c143f3b62ea50d54f56e449dd3f2
WTreyde <wojtek.treyde@t-online.de>
2025-02-21 11:02:15 +0000

    Update __init__.py

--- astra/__init__.py
+++ astra/__init__.py
@@ -1,7 +1,5 @@
-"""Automated model selection using statistical testing for robust algorithms"""
-
-# Add imports here
 from .model_selection import *
-
-
+from .models import *
+from .data import *
+from .featurisation import *
 from ._version import __version__
